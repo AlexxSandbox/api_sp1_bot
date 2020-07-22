2846190cab3d0a8c0b4d2b75038b1d3cdb047f93
Aleksandr_Sergeev <alexx.sergeev@gmail.com>
2020-07-22 23:10:02 +0500

    homework statuses check

--- homework.py
+++ homework.py
@@ -7,34 +7,41 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+PRAKTIKUM_URL = 'https://praktikum.yandex.ru/api/user_api/{}/'
+
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-PRAKTIKUM_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+TELEGRAM_URL = 'https://api.telegram.org/bot{}/{}'
 
 logging.basicConfig(filename='log.txt', level=logging.ERROR, format='%(name)s - %(asctime)s - %(message)s')
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    if homework['status'] == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    if 'homework_name' in homework and 'status' in homework:
+        homework_name = homework['homework_name']
+        homework_status = homework['status']
+
+        if homework_status == 'rejected':
+            verdict = 'К сожалению в работе нашлись ошибки.'
+        elif homework_status == 'approved':
+            verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        else:
+            verdict = 'Но статус получить не удалось. Свяжитесь с ревьювером.'
+
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
     log = logging.getLogger('Praktikum')
     headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-    params = {'from_date': 0}
+    params = {'from_date': current_timestamp}
+    request_url = PRAKTIKUM_URL.format('homework_statuses')
     try:
-        homework_statuses = requests.get(PRAKTIKUM_URL, headers=headers, params=params).json()
+        homework_statuses = requests.get(request_url, headers=headers, params=params).json()
 
         if 'homeworks' in homework_statuses:
             return homework_statuses
-
         elif 'message' in homework_statuses:
             error_msg = homework_statuses['message']
             raise KeyError(f'{error_msg}')
